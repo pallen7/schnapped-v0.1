98e84ab69d5c8ba08a6d498633caafc8104df782
pallen7 <paulallen7@msn.com>
2016-01-10 00:41:23 +0000

    More basics

---
+++ Schnap.Client/Controllers/PicturesController.cs
@@ -0,0 +1,24 @@
+﻿using Schnap.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Schnap.Client.Controllers
+{
+    public class PicturesController : Controller
+    {
+        private IPictureRepository picture_repository;
+
+        public PicturesController(IPictureRepository picture_repository)
+        {
+            this.picture_repository = picture_repository;
+        }
+
+        public ActionResult List()
+        {
+            return View(picture_repository.pictures);
+        }
+    }
+}
